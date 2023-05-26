9bfdf475222901f5fe7edcb5760b6dd8c56a5f50
sadums <samjadams4@gmail.com>
2023-05-26 16:31:11 -0600

    I hate the steam api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,13 @@ const userRoutes = require('./user-routes');
 const rawgRoutes = require('./rawg-routes');
 const steamRoutes = require('./steam-routes');
 
+
+// proxy server to resolve CORS requests
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 router.use('/user', userRoutes);
 router.use('/rawg', rawgRoutes);
 router.use('/steam', steamRoutes);
