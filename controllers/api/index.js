697312680c9ec74a437fd9c9c6d34acc0da6539f
jonny desktop <jon@uslms.com>
2023-05-26 17:10:37 -0600

    added api routes so the database data can be used on the front end

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
 
+//database
 const userRoutes = require('./user-routes');
+const userGamesRoutes = require('./usergames-routes');
+const userFriendsRoutes = require('./userfriends-routes');
+const gameRoutes = require('./game-routes');
+
+router.use('/user', userRoutes);
+router.use('/usergames', userGamesRoutes);
+router.use('/userfriends', userFriendsRoutes);
+router.use('/game', gameRoutes);
+
+
+//external apis
 const rawgRoutes = require('./rawg-routes');
 const steamRoutes = require('./steam-routes');
 
-router.use('/user', userRoutes);
 router.use('/rawg', rawgRoutes);
 router.use('/steam', steamRoutes);
+
+
+
 
 module.exports = router;
