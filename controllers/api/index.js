c33d521d1aad51e29f4c3b5c473e14cf1063e3c1
sadums <samjadams4@gmail.com>
2023-05-26 11:13:00 -0600

    added steam and rawg route files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const rawgRoutes = require('./rawg-routes');
+const steamRoutes = require('./steam-routes');
 
 router.use('/user', userRoutes);
+router.use('/rawg', rawgRoutes);
+router.use('/steam', steamRoutes);
 
 module.exports = router;
---
+++ controllers/api/rawg-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+require('dotenv').config();
+
+
+const apiKey = process.env.API_KEY_RAWG;
+
+router.get('/:game', async(req, res) => {
+    
+});
+
+
+
+
+module.exports = router;
