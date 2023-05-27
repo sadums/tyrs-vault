a5c31acd04710b64fd8cd6412813a1afc83f809e
sadums <samjadams4@gmail.com>
2023-05-26 23:58:19 -0600

    added error catching to rawg routes

--- controllers/api/rawg-routes.js
+++ controllers/api/rawg-routes.js
@@ -3,31 +3,46 @@ const axios = require('axios');
 
 require('dotenv').config();
 
+const apiKey = process.env.API_KEY_RAWG;
+
 // ENDPOINT: "/api/rawg/"
 
-const apiKey = process.env.API_KEY_RAWG;
 
 
 // Receive a list of games
 // ENDPOINT: "/api/rawg/games"
 router.get('/games', async(req, res) => {
-    const url = `https://api.rawg.io/api/games?key=${apiKey}"`;
-    axios.get(url)
-    .then((response) => {
-        console.log(response.data);
-        res.json(response.data);
-    });
+    try{
+        const url = `https://api.rawg.io/api/games?key=${apiKey}"`;
+        axios.get(url)
+        .then((response) => {
+            console.log(response.data);
+            res.json(response.data);
+        });
+    }catch(e){
+        res.status(500).json({
+            message: "Something went wrong.",
+            error: e
+        });
+    }
 });
 
 // Search for a specific game
 // ENDPOINT: '/api/rawg/search/:game"
 router.get('/search/:game', async(req, res) => {
-    const url = `https://api.rawg.io/api/games?key=${apiKey}&search="${req.params.game}"`;
-    axios.get(url)
-    .then((response) => {
-        console.log(response.data);
-        res.json(response.data);
-    });
+    try{
+        const url = `https://api.rawg.io/api/games?key=${apiKey}&search="${req.params.game}"`;
+        axios.get(url)
+        .then((response) => {
+            console.log(response.data);
+            res.json(response.data);
+        });
+    }catch(e){
+        res.status(500).json({
+            message: "Something went wrong.",
+            error: e
+        });
+    }
 });
 
 module.exports = router;
