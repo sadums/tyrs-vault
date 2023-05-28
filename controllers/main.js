e3e76ad82e81003083967f395b73ba7f3953da57
sadums <samjadams4@gmail.com>
2023-05-27 18:13:37 -0600

    created profile routes

--- controllers/main.js
+++ controllers/main.js
@@ -29,29 +29,50 @@ router.get('/login', (req, res) => {
     }
 });
 
-router.get('/friends', (req, res) => {
+router.get('/friends', async (req, res) => {
     try{
-        if(req.session.loggedIn){
-            res.render('friends');
+        if(!req.session.loggedIn){
+            res.redirect('/');
             return;
         }
 
-        res.redirect('/');
+        res.render('friends');
     }catch(e){
         console.error(e);
         res.status(500).json(e);
     }
 });
 
-router.get('/test', async(req, res) => {
-    try{
-        res.render('test',{
-            testing: true
-        });
-    }catch(e){
-        console.error(e);
-        res.status(500).json(e);
+// Renders the users profile
+router.get('/profile', async (req, res) => {
+    if(!req.session.loggedIn){
+        res.redirect('/');
+        return;
     }
+
+    const user = await User.findByPk(req.session.userid);
+    if(!user){
+        res.status(404).json({ message : "Something went wrong, please try again"});
+        return;
+    }
+    res.render('profile', {
+        user: user.dataValues
+    });
 });
+
+// Render a specific users profile
+router.get('/profile/:id', async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+
+    if(!user){
+        res.status(404).json({ message : "Something went wrong, please try again"});
+        return;
+    }
+    res.render('profile', {
+        user: user.dataValues
+    });
+});
+
+
 
 module.exports = router
