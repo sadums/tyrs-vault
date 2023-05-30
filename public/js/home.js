571b5fd82c92aa0ff691d5f9f7b5b5f267cf1f16
Carter <carreejoh4637@gmail.com>
2023-05-29 18:56:37 -0600

    Accidentally worked on main should be no problesm though

--- public/js/home.js
+++ public/js/home.js
@@ -16,16 +16,57 @@ const logout = async() => {
 logoutButton.addEventListener('click', logout);
 
 
-// const renderUserList = async() => {
-//     const status = await fetch('/', {
-//         method: 'GET'
-//     });
-//     if(status.ok) {
-//         let users = document.querySelector('.userListItems');
-//         users.classList.add("splide__slide");
-//         console.log("SDFSDFDSFSDFsd");
-//     }
-// }
 
-// window.addEventListener('load', renderUserList, console.log("Im reached"));
 
+
+
+
+let mainFriendSplide = document.querySelector('#friendDisplay');
+
+
+        function fetchUserData () {
+    
+        fetch('/api/user', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            appendFriendData(data);
+        });
+        };
+    
+        function appendFriendData(data) {
+            console.log(data);
+
+            for(i=0; i<data.length; i++) {
+            console.log(data[i]);
+
+            let friendSlide = document.createElement("li");
+            let friendDiv = document.createElement("div");
+            let friendUsername = document.createElement("h2");
+            
+            friendSlide.className = "splide__slide";
+            friendDiv.className = "friendCard";
+
+            mainFriendSplide.appendChild(friendSlide);
+            friendSlide.appendChild(friendDiv);
+            friendDiv.appendChild(friendUsername);
+
+            friendUsername.innerHTML = data[i].username;
+            };
+
+            let splide1 = document.querySelector("#splide1");
+            new Splide( splide1, {
+                focus: 'center',
+               } ).mount();
+        };
+
+
+    fetchUserData();
+    
+
+  
+    
