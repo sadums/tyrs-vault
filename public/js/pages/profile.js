f1297513cbb7a894cd73b5767cedc8ebc1af973f
jonny desktop <jon@uslms.com>
2023-06-03 13:19:21 -0600

    logout buttons works on all pages

--- public/js/pages/profile.js
+++ public/js/pages/profile.js
@@ -75,3 +75,20 @@ const pfpInput = document.getElementById('imageInput');
 pfpInput.addEventListener('change', (event) => {
     pfpChange(event);
 });
+
+
+//All logout logic
+const logoutButton = document.getElementById('logout');
+const logout = async () => {
+    const response = await fetch('/api/user/logout', {
+        method: 'POST'
+    });
+
+    if (response.ok) {
+        document.location.replace('/login');
+    } else {
+        alert('Something went wrong, please try again');
+    }
+}
+
+logoutButton.addEventListener('click', logout);
